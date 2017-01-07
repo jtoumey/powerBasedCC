46b416008ca2f02ffe84a14b5f0f189d6da2971b
Julian Toumey <jtoumey@gmail.com>
2017-01-07 09:41:10 -0600

    Updated sampleDriver to write the list to file; file is named with system time

--- test/attemptJT/sampleDriver.py
+++ test/attemptJT/sampleDriver.py
@@ -1,5 +1,6 @@
 IDX = 12
 
+import time
 # Import class definitions from the class definition file
 import classDef
 
@@ -12,6 +13,8 @@ for i in range(IDX):
    t = classDef.sample() 
    # Assign the time as the current iteration counter
    t.time = i*i
+   t.heartRate = i + 140
+   t.power = i*10 +43
    sampleList.append(t)
 
 
@@ -19,3 +22,18 @@ for i in range(IDX):
 
 for i in range(IDX):
    print("{}".format(sampleList[i].time))
+
+
+timestr = time.strftime("%Y-%m-%d-%H:%M")
+timestr += ".dat"
+
+outputFile = open(timestr, 'w')
+
+# Print a file header
+outputFile.write("# Sampled Data\n")
+outputFile.write("# time (sec)   heart rate (BPM)   power (Watt)\n")
+
+for i in range(IDX):
+   outputFile.write("{} {} {}\n".format(sampleList[i].time, sampleList[i].heartRate, sampleList[i].power))
+
+outputFile.close()
